980a5593e9273db6cc430549cceb2c6ce95274d1
ohr486 <ohr486@gmail.com>
2020-10-15 23:15:23 +0900

    mv docker files

--- app/config/dev.exs
+++ app/config/dev.exs
@@ -1,11 +1,10 @@
 use Mix.Config
 
+database_url = System.get_env("DATABASE_URL")
+
 # Configure your database
 config :devops_meetup, DevopsMeetup.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "devops_meetup_dev",
-  hostname: "localhost",
+  url: database_url,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
